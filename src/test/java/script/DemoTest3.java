41913916fca7cbd503a19eb340cd5e99fb1033d8
LDasara <LDasara@HIBAML131327.ad.harman.com>
2023-05-03 12:45:39 +0530

    Draft copy

---
+++ src/test/java/script/DemoTest3.java
@@ -0,0 +1,17 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class DemoTest3 extends BaseTest
+{
+	@Test
+	public void demoTest3() {
+			
+			Reporter.log(driver.getTitle(),true);	
+
+	}
+}
+
