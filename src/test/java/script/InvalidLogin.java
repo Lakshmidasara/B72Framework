b78c399cc3d51feb316219b3b60afae7565b1467
LDasara <LDasara@HIBAML131327.ad.harman.com>
2023-05-05 11:33:45 +0530

    e

--- src/test/java/script/InvalidLogin.java
+++ src/test/java/script/InvalidLogin.java
@@ -12,17 +12,21 @@ public class InvalidLogin extends BaseTest
 	@Test(priority = 2)
 	public void testInvalidLogin()
 	{
-		String un=Excel.getData(XL_PATH,"InvalidLogin", 1, 0);
-		String pw=Excel.getData(XL_PATH,"InvalidLogin", 1, 1);
-//		1. enter invalid un
-		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName(un);
-//		2. enter invalid pw
-		loginPage.setPassword(pw);
-//		3. click login button
-		loginPage.clickLoginButton();
-//		4. err msg should be displayed
-		boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
-		Assert.assertTrue(result);
+		int rc=Excel.getRowCount(XL_PATH, "InvalidLogin");
+		for(int i=1;i<=rc;i++)
+		{
+			String un=Excel.getData(XL_PATH,"InvalidLogin", i, 0);
+			String pw=Excel.getData(XL_PATH,"InvalidLogin", i, 1);
+	//		1. enter invalid un
+			LoginPage loginPage=new LoginPage(driver);
+			loginPage.setUserName(un);
+	//		2. enter invalid pw
+			loginPage.setPassword(pw);
+	//		3. click login button
+			loginPage.clickLoginButton();
+	//		4. err msg should be displayed
+			boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
+			Assert.assertTrue(result);
+		}
 	}
 }
